4adf5403b78ba51cb2a2d6e56bdf15f6f1a489e4
Michael Kriese <michael.kriese@visualon.de>
2022-05-18 15:38:28 +0200

    fix: force directory creation

--- windows/src/tools/bin/prepare-user.ps1
+++ windows/src/tools/bin/prepare-user.ps1
@@ -16,7 +16,7 @@ $ErrorActionPreference = "Stop"
 "$Name"  | Write-Host -ForegroundColor Green -NoNewline
 " ... "  | Write-Host -ForegroundColor DarkGray
 
-New-Item C:\Users\$Name\AppData\Roaming -ItemType Directory | Out-Null
-New-Item C:\Users\$Name\AppData\Local\Temp -ItemType Directory | Out-Null
+New-Item C:\Users\$Name\AppData\Roaming -ItemType Directory -Force | Out-Null
+New-Item C:\Users\$Name\AppData\Local\Temp -ItemType Directory -Force | Out-Null
 
 " done." | Write-Host -ForegroundColor DarkGray
