62237b5602c3316cb4e3c9773bdc285c4f3df86a
Michael Kriese <michael.kriese@visualon.de>
2023-03-29 11:40:57 +0200

    chore: more testing

--- windows/test/run.ps1
+++ windows/test/run.ps1
@@ -7,6 +7,10 @@ $DebugPreference = 'Continue'
 
 exec { msbuild --version }
 
-exec { npm i --no-package-lock }
+exec { npm i --no-package-lock --no-audit --no-fund --silent }
+
+Get-ChildItem node_modules
+Get-ChildItem node_modules\@swc
+Get-ChildItem node_modules\@swc\core-win32-x64-msvc
 
 exec { node -e 'require("@swc/core-win32-x64-msvc")' }
