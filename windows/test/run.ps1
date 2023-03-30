2e850c0eb46013d2a2a746e295c3d006b8d45f40
Michael Kriese <michael.kriese@visualon.de>
2023-03-30 14:33:21 +0200

    Apply suggestions from code review

--- windows/test/run.ps1
+++ windows/test/run.ps1
@@ -9,8 +9,4 @@ exec { msbuild --version }
 
 exec { npm i --no-package-lock --no-audit --no-fund --silent }
 
-Get-ChildItem node_modules
-Get-ChildItem node_modules\@swc
-Get-ChildItem node_modules\@swc\core-win32-x64-msvc
-
 exec { node -e 'require("@swc/core-win32-x64-msvc")' }
